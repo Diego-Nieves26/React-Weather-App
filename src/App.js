181714c2db8c7fe755904867ae13f1a5af8f58e1
Diego-Nieves26 <nieves.diego0426@gmail.com>
2022-06-17 17:36:41 -0500

    add: style

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import './App.css';
-import { useState } from 'react';
-import Information from './components/Information';
-import useBringApi from './hooks/useBringApi';
+import "./App.css";
+import { Loader, Information } from "./components";
 
 function App() {
-  //Degrees Cº/Fº
-  const [celsiusDegrees, setCelsiusDegrees] = useState(true)
-  const changeGrade = () => setCelsiusDegrees(!celsiusDegrees)
-  //Api
-  const {dataApi, degrees} = useBringApi()
   return (
     <>
-      <div className="box">
-        <h1>Wheather App</h1>
-        <h2>{dataApi.sys?.country + " " + dataApi.name}</h2>
-        <div className="box-content">
-          <div>
-            <img src={`http://openweathermap.org/img/wn/${dataApi.weather?.[0].icon}@2x.png`} alt="Img" />
-            <h1>{celsiusDegrees ? `${(degrees - 273.15).toFixed(2)} Cº`: `${((degrees - 273.15) * 1.8 +32).toFixed(2)} Fº`}</h1>
-          </div>
-          <Information description={dataApi.weather?.[0].description} speed={dataApi.wind?.speed} all={dataApi.clouds?.all} gust={dataApi.wind?.gust}/>
-        </div>
-        <button onClick={changeGrade}>Degrees ºF/ºC</button>
-      </div>
+      <Loader />
+      <Information />
     </>
   );
 }
--- src/components/Information.js
+++ src/components/Information.js
@@ -1,14 +1,70 @@
-const Information = ({description, speed, all, gust}) => {
-    return (
-        <div>
-            <ul>
-                <li><span>"{description}"</span></li>
-                <li><span>Wind speed: </span>{speed} m/s</li>
-                <li><span>Clouds: </span>{all}%</li>
-                <li><span>Gust: </span>{gust} mb</li>
-            </ul>
+import { useState } from "react";
+import useBringApi from "../hooks/useBringApi";
+import { motion } from "framer-motion";
+
+const Information = () => {
+  //Degrees Cº/Fº
+  const [celsiusDegrees, setCelsiusDegrees] = useState(true);
+  const changeGrade = () => setCelsiusDegrees(!celsiusDegrees);
+  //Api
+  const { dataApi } = useBringApi();
+  return (
+    <motion.div
+      className="box"
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 20,
+        delay: 0.5,
+      }}
+    >
+      <section className="title">Wheather App</section>
+      <section className="info">
+        <h2>{dataApi.location?.name + " " + dataApi.location?.region}</h2>
+        <h3>{dataApi.location?.country}</h3>
+        <div className="box-content">
+          <img src={dataApi.current?.condition.icon} alt="Img" />
+          <h3>
+            {celsiusDegrees
+              ? dataApi.current?.temp_c + "ºC"
+              : dataApi.current.temp_f + "ºF"}
+            <br />
+            {dataApi.current?.condition.text}
+          </h3>
         </div>
-    );
+        <ul>
+          <li>
+            <span>"{dataApi.location?.localtime}"</span>
+          </li>
+          <li>
+            <span>Humidity: </span>
+            {dataApi.current?.humidity} m/s
+          </li>
+          <li>
+            <span>Clouds: </span>
+            {dataApi.current?.cloud}%
+          </li>
+          <li>
+            <span>Gust: </span>
+            {dataApi.current?.gust_mph} mb
+          </li>
+        </ul>
+        <motion.button
+          onClick={changeGrade}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{
+            scale: 0.9,
+            rotate: 5,
+            borderRadius: "30px",
+          }}
+        >
+          Degrees ºF/ºC
+        </motion.button>
+      </section>
+    </motion.div>
+  );
 };
 
 export default Information;
